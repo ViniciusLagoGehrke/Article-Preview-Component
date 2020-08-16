0a37c22776cdd13e1d287f4a9e0e42bd3f218e58
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-08-16 00:23:49 +0000

    finished desktop layout and started JS

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-no scrip yet
+const $btnShare = document.querySelector(.btnShare)
+const $menuFront = document.querySelector(.front)
+const $menuBack = document.querySelector(.back)
+
+$btnShare.addEventListener('click',function(){
+  $menuFront.classList.add('hidden')
+  $menuBack.classList.remove('hidden')
+})
